3077b9bd967554ade04251cd8a551d036648bab8
cyrillic7 <121888719@qq.com>
2026-05-08 09:24:03 +0800

    udpate .gitignore

--- build/moc/moc_mainwindow.cpp
+++ build/moc/moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[46];
-    char stringdata0[821];
+    QByteArrayData data[49];
+    char stringdata0[883];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -50,33 +50,36 @@ QT_MOC_LITERAL(15, 202, 12), // "onMenuAction"
 QT_MOC_LITERAL(16, 215, 15), // "onToggleLogView"
 QT_MOC_LITERAL(17, 231, 19), // "onToggleCommandView"
 QT_MOC_LITERAL(18, 251, 26), // "onToggleBuiltinCommandView"
-QT_MOC_LITERAL(19, 278, 20), // "onVerifyFileFinished"
-QT_MOC_LITERAL(20, 299, 17), // "onShowMachineCode"
-QT_MOC_LITERAL(21, 317, 19), // "onUpgradeQtSoftware"
-QT_MOC_LITERAL(22, 337, 16), // "onUpgrade7evEmmc"
-QT_MOC_LITERAL(23, 354, 14), // "onUpgrade7evSd"
-QT_MOC_LITERAL(24, 369, 13), // "onUpgradeKu5p"
-QT_MOC_LITERAL(25, 383, 22), // "onExecuteCustomCommand"
-QT_MOC_LITERAL(26, 406, 20), // "onClearCommandOutput"
-QT_MOC_LITERAL(27, 427, 26), // "onCommandInputEnterPressed"
-QT_MOC_LITERAL(28, 454, 14), // "onOpenSettings"
-QT_MOC_LITERAL(29, 469, 15), // "onManageSSHKeys"
-QT_MOC_LITERAL(30, 485, 16), // "onGenerateSSHKey"
-QT_MOC_LITERAL(31, 502, 15), // "onCopyPublicKey"
-QT_MOC_LITERAL(32, 518, 18), // "onInstallPublicKey"
-QT_MOC_LITERAL(33, 537, 14), // "onDeleteSSHKey"
-QT_MOC_LITERAL(34, 552, 19), // "onSSHKeyGenFinished"
-QT_MOC_LITERAL(35, 572, 25), // "onGenerateAndDeploySSHKey"
-QT_MOC_LITERAL(36, 598, 14), // "onEnableSSHKey"
-QT_MOC_LITERAL(37, 613, 15), // "onDisableSSHKey"
-QT_MOC_LITERAL(38, 629, 23), // "onExecuteBuiltinCommand"
-QT_MOC_LITERAL(39, 653, 21), // "onClearBuiltinCommand"
-QT_MOC_LITERAL(40, 675, 20), // "onClearBuiltinOutput"
-QT_MOC_LITERAL(41, 696, 33), // "onBuiltinCommandInputEnterPre..."
-QT_MOC_LITERAL(42, 730, 27), // "onPasswordInputEnterPressed"
-QT_MOC_LITERAL(43, 758, 23), // "onPasswordInputFinished"
-QT_MOC_LITERAL(44, 782, 23), // "onPasswordInputCanceled"
-QT_MOC_LITERAL(45, 806, 14) // "onDeploySSHKey"
+QT_MOC_LITERAL(19, 278, 25), // "onToggleFileSelectionView"
+QT_MOC_LITERAL(20, 304, 20), // "onToggleSettingsView"
+QT_MOC_LITERAL(21, 325, 20), // "onVerifyFileFinished"
+QT_MOC_LITERAL(22, 346, 17), // "onShowMachineCode"
+QT_MOC_LITERAL(23, 364, 19), // "onUpgradeQtSoftware"
+QT_MOC_LITERAL(24, 384, 16), // "onUpgrade7evEmmc"
+QT_MOC_LITERAL(25, 401, 14), // "onUpgrade7evSd"
+QT_MOC_LITERAL(26, 416, 13), // "onUpgradeKu5p"
+QT_MOC_LITERAL(27, 430, 14), // "onUpgradePatch"
+QT_MOC_LITERAL(28, 445, 22), // "onExecuteCustomCommand"
+QT_MOC_LITERAL(29, 468, 20), // "onClearCommandOutput"
+QT_MOC_LITERAL(30, 489, 26), // "onCommandInputEnterPressed"
+QT_MOC_LITERAL(31, 516, 14), // "onOpenSettings"
+QT_MOC_LITERAL(32, 531, 15), // "onManageSSHKeys"
+QT_MOC_LITERAL(33, 547, 16), // "onGenerateSSHKey"
+QT_MOC_LITERAL(34, 564, 15), // "onCopyPublicKey"
+QT_MOC_LITERAL(35, 580, 18), // "onInstallPublicKey"
+QT_MOC_LITERAL(36, 599, 14), // "onDeleteSSHKey"
+QT_MOC_LITERAL(37, 614, 19), // "onSSHKeyGenFinished"
+QT_MOC_LITERAL(38, 634, 25), // "onGenerateAndDeploySSHKey"
+QT_MOC_LITERAL(39, 660, 14), // "onEnableSSHKey"
+QT_MOC_LITERAL(40, 675, 15), // "onDisableSSHKey"
+QT_MOC_LITERAL(41, 691, 23), // "onExecuteBuiltinCommand"
+QT_MOC_LITERAL(42, 715, 21), // "onClearBuiltinCommand"
+QT_MOC_LITERAL(43, 737, 20), // "onClearBuiltinOutput"
+QT_MOC_LITERAL(44, 758, 33), // "onBuiltinCommandInputEnterPre..."
+QT_MOC_LITERAL(45, 792, 27), // "onPasswordInputEnterPressed"
+QT_MOC_LITERAL(46, 820, 23), // "onPasswordInputFinished"
+QT_MOC_LITERAL(47, 844, 23), // "onPasswordInputCanceled"
+QT_MOC_LITERAL(48, 868, 14) // "onDeploySSHKey"
 
     },
     "MainWindow\0onSelectFile\0\0onUploadFile\0"
@@ -87,9 +90,11 @@ QT_MOC_LITERAL(45, 806, 14) // "onDeploySSHKey"
     "onUploadTimeout\0onTestFinished\0"
     "onMenuAction\0onToggleLogView\0"
     "onToggleCommandView\0onToggleBuiltinCommandView\0"
-    "onVerifyFileFinished\0onShowMachineCode\0"
-    "onUpgradeQtSoftware\0onUpgrade7evEmmc\0"
-    "onUpgrade7evSd\0onUpgradeKu5p\0"
+    "onToggleFileSelectionView\0"
+    "onToggleSettingsView\0onVerifyFileFinished\0"
+    "onShowMachineCode\0onUpgradeQtSoftware\0"
+    "onUpgrade7evEmmc\0onUpgrade7evSd\0"
+    "onUpgradeKu5p\0onUpgradePatch\0"
     "onExecuteCustomCommand\0onClearCommandOutput\0"
     "onCommandInputEnterPressed\0onOpenSettings\0"
     "onManageSSHKeys\0onGenerateSSHKey\0"
@@ -111,7 +116,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-      41,   14, // methods
+      44,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -119,47 +124,50 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,  219,    2, 0x08 /* Private */,
-       3,    0,  220,    2, 0x08 /* Private */,
-       4,    0,  221,    2, 0x08 /* Private */,
-       5,    0,  222,    2, 0x08 /* Private */,
-       6,    0,  223,    2, 0x08 /* Private */,
-       7,    2,  224,    2, 0x08 /* Private */,
-      11,    0,  229,    2, 0x08 /* Private */,
-      12,    0,  230,    2, 0x08 /* Private */,
-      13,    0,  231,    2, 0x08 /* Private */,
-      14,    2,  232,    2, 0x08 /* Private */,
-      15,    0,  237,    2, 0x08 /* Private */,
-      16,    0,  238,    2, 0x08 /* Private */,
-      17,    0,  239,    2, 0x08 /* Private */,
-      18,    0,  240,    2, 0x08 /* Private */,
-      19,    2,  241,    2, 0x08 /* Private */,
-      20,    0,  246,    2, 0x08 /* Private */,
-      21,    0,  247,    2, 0x08 /* Private */,
-      22,    0,  248,    2, 0x08 /* Private */,
-      23,    0,  249,    2, 0x08 /* Private */,
-      24,    0,  250,    2, 0x08 /* Private */,
-      25,    0,  251,    2, 0x08 /* Private */,
-      26,    0,  252,    2, 0x08 /* Private */,
-      27,    0,  253,    2, 0x08 /* Private */,
-      28,    0,  254,    2, 0x08 /* Private */,
-      29,    0,  255,    2, 0x08 /* Private */,
-      30,    0,  256,    2, 0x08 /* Private */,
-      31,    0,  257,    2, 0x08 /* Private */,
-      32,    0,  258,    2, 0x08 /* Private */,
-      33,    0,  259,    2, 0x08 /* Private */,
-      34,    2,  260,    2, 0x08 /* Private */,
-      35,    0,  265,    2, 0x08 /* Private */,
-      36,    0,  266,    2, 0x08 /* Private */,
-      37,    0,  267,    2, 0x08 /* Private */,
-      38,    0,  268,    2, 0x08 /* Private */,
-      39,    0,  269,    2, 0x08 /* Private */,
-      40,    0,  270,    2, 0x08 /* Private */,
-      41,    0,  271,    2, 0x08 /* Private */,
-      42,    0,  272,    2, 0x08 /* Private */,
-      43,    0,  273,    2, 0x08 /* Private */,
-      44,    0,  274,    2, 0x08 /* Private */,
-      45,    0,  275,    2, 0x08 /* Private */,
+       1,    0,  234,    2, 0x08 /* Private */,
+       3,    0,  235,    2, 0x08 /* Private */,
+       4,    0,  236,    2, 0x08 /* Private */,
+       5,    0,  237,    2, 0x08 /* Private */,
+       6,    0,  238,    2, 0x08 /* Private */,
+       7,    2,  239,    2, 0x08 /* Private */,
+      11,    0,  244,    2, 0x08 /* Private */,
+      12,    0,  245,    2, 0x08 /* Private */,
+      13,    0,  246,    2, 0x08 /* Private */,
+      14,    2,  247,    2, 0x08 /* Private */,
+      15,    0,  252,    2, 0x08 /* Private */,
+      16,    0,  253,    2, 0x08 /* Private */,
+      17,    0,  254,    2, 0x08 /* Private */,
+      18,    0,  255,    2, 0x08 /* Private */,
+      19,    0,  256,    2, 0x08 /* Private */,
+      20,    0,  257,    2, 0x08 /* Private */,
+      21,    2,  258,    2, 0x08 /* Private */,
+      22,    0,  263,    2, 0x08 /* Private */,
+      23,    0,  264,    2, 0x08 /* Private */,
+      24,    0,  265,    2, 0x08 /* Private */,
+      25,    0,  266,    2, 0x08 /* Private */,
+      26,    0,  267,    2, 0x08 /* Private */,
+      27,    0,  268,    2, 0x08 /* Private */,
+      28,    0,  269,    2, 0x08 /* Private */,
+      29,    0,  270,    2, 0x08 /* Private */,
+      30,    0,  271,    2, 0x08 /* Private */,
+      31,    0,  272,    2, 0x08 /* Private */,
+      32,    0,  273,    2, 0x08 /* Private */,
+      33,    0,  274,    2, 0x08 /* Private */,
+      34,    0,  275,    2, 0x08 /* Private */,
+      35,    0,  276,    2, 0x08 /* Private */,
+      36,    0,  277,    2, 0x08 /* Private */,
+      37,    2,  278,    2, 0x08 /* Private */,
+      38,    0,  283,    2, 0x08 /* Private */,
+      39,    0,  284,    2, 0x08 /* Private */,
+      40,    0,  285,    2, 0x08 /* Private */,
+      41,    0,  286,    2, 0x08 /* Private */,
+      42,    0,  287,    2, 0x08 /* Private */,
+      43,    0,  288,    2, 0x08 /* Private */,
+      44,    0,  289,    2, 0x08 /* Private */,
+      45,    0,  290,    2, 0x08 /* Private */,
+      46,    0,  291,    2, 0x08 /* Private */,
+      47,    0,  292,    2, 0x08 /* Private */,
+      48,    0,  293,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -176,7 +184,10 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void, QMetaType::Int, 0x80000000 | 9,    8,   10,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -227,33 +238,36 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 11: _t->onToggleLogView(); break;
         case 12: _t->onToggleCommandView(); break;
         case 13: _t->onToggleBuiltinCommandView(); break;
-        case 14: _t->onVerifyFileFinished((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< QProcess::ExitStatus(*)>(_a[2]))); break;
-        case 15: _t->onShowMachineCode(); break;
-        case 16: _t->onUpgradeQtSoftware(); break;
-        case 17: _t->onUpgrade7evEmmc(); break;
-        case 18: _t->onUpgrade7evSd(); break;
-        case 19: _t->onUpgradeKu5p(); break;
-        case 20: _t->onExecuteCustomCommand(); break;
-        case 21: _t->onClearCommandOutput(); break;
-        case 22: _t->onCommandInputEnterPressed(); break;
-        case 23: _t->onOpenSettings(); break;
-        case 24: _t->onManageSSHKeys(); break;
-        case 25: _t->onGenerateSSHKey(); break;
-        case 26: _t->onCopyPublicKey(); break;
-        case 27: _t->onInstallPublicKey(); break;
-        case 28: _t->onDeleteSSHKey(); break;
-        case 29: _t->onSSHKeyGenFinished((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< QProcess::ExitStatus(*)>(_a[2]))); break;
-        case 30: _t->onGenerateAndDeploySSHKey(); break;
-        case 31: _t->onEnableSSHKey(); break;
-        case 32: _t->onDisableSSHKey(); break;
-        case 33: _t->onExecuteBuiltinCommand(); break;
-        case 34: _t->onClearBuiltinCommand(); break;
-        case 35: _t->onClearBuiltinOutput(); break;
-        case 36: _t->onBuiltinCommandInputEnterPressed(); break;
-        case 37: _t->onPasswordInputEnterPressed(); break;
-        case 38: _t->onPasswordInputFinished(); break;
-        case 39: _t->onPasswordInputCanceled(); break;
-        case 40: _t->onDeploySSHKey(); break;
+        case 14: _t->onToggleFileSelectionView(); break;
+        case 15: _t->onToggleSettingsView(); break;
+        case 16: _t->onVerifyFileFinished((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< QProcess::ExitStatus(*)>(_a[2]))); break;
+        case 17: _t->onShowMachineCode(); break;
+        case 18: _t->onUpgradeQtSoftware(); break;
+        case 19: _t->onUpgrade7evEmmc(); break;
+        case 20: _t->onUpgrade7evSd(); break;
+        case 21: _t->onUpgradeKu5p(); break;
+        case 22: _t->onUpgradePatch(); break;
+        case 23: _t->onExecuteCustomCommand(); break;
+        case 24: _t->onClearCommandOutput(); break;
+        case 25: _t->onCommandInputEnterPressed(); break;
+        case 26: _t->onOpenSettings(); break;
+        case 27: _t->onManageSSHKeys(); break;
+        case 28: _t->onGenerateSSHKey(); break;
+        case 29: _t->onCopyPublicKey(); break;
+        case 30: _t->onInstallPublicKey(); break;
+        case 31: _t->onDeleteSSHKey(); break;
+        case 32: _t->onSSHKeyGenFinished((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< QProcess::ExitStatus(*)>(_a[2]))); break;
+        case 33: _t->onGenerateAndDeploySSHKey(); break;
+        case 34: _t->onEnableSSHKey(); break;
+        case 35: _t->onDisableSSHKey(); break;
+        case 36: _t->onExecuteBuiltinCommand(); break;
+        case 37: _t->onClearBuiltinCommand(); break;
+        case 38: _t->onClearBuiltinOutput(); break;
+        case 39: _t->onBuiltinCommandInputEnterPressed(); break;
+        case 40: _t->onPasswordInputEnterPressed(); break;
+        case 41: _t->onPasswordInputFinished(); break;
+        case 42: _t->onPasswordInputCanceled(); break;
+        case 43: _t->onDeploySSHKey(); break;
         default: ;
         }
     }
@@ -288,13 +302,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 41)
+        if (_id < 44)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 41;
+        _id -= 44;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 41)
+        if (_id < 44)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 41;
+        _id -= 44;
     }
     return _id;
 }
